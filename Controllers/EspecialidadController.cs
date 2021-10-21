8f1e2ad971bc7622a76e4bb2ca9921da266f604d
Israeli Perez <israeli.ps@gmail.com>
2021-10-21 14:38:47 -0500

    agregando conexion a BD

---
+++ Controllers/EspecialidadController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Turnos.Controllers{
+    public class EspecialidadController:Controller
+    {
+        public EspecialidadController()
+        {
+            
+        }
+
+        public IActionResult Index(){
+            return View();
+        }
+    }
+}
